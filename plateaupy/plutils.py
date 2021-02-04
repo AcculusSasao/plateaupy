2e0927217d4a2fdbc659774eaf31c2513ea8365e
y.sasao <sasao@acculus.jp>
2021-02-04 11:39:06 +0900

    add 2 functions in plutils

--- plateaupy/plutils.py
+++ plateaupy/plutils.py
@@ -2,6 +2,8 @@ import math
 import numpy as np
 import random
 import string
+import copy
+import cv2
 
 def printMethods(obj):
 	for x in dir(obj):
@@ -19,23 +21,106 @@ def str2floats(x):
 # ref: https://vldb.gsi.go.jp/sokuchi/surveycalc/surveycalc/transf.html
 # ref: https://vldb.gsi.go.jp/sokuchi/surveycalc/surveycalc/algorithm/trans/trans_alg.html
 # ref: http://tancro.e-central.tv/grandmaster/excel/radius.html
-def convertPolarToCartsian( lon, lat, hei ):
-	cosLon = math.cos( lon * math.pi/180 )
-	sinLon = math.sin( lon * math.pi/180 )
+def convertPolarToCartsian( lat, lon, hei ):
 	cosLat = math.cos( lat * math.pi/180 )
 	sinLat = math.sin( lat * math.pi/180 )
+	cosLon = math.cos( lon * math.pi/180 )
+	sinLon = math.sin( lon * math.pi/180 )
 	# Semi-mejor axis  [in meter]
 	a = 6378137
 	# Flattening
 	f = 1 / 298.257222101
 	# Eccentricity
 	e = math.sqrt( 2*f - f*f )
-	W = math.sqrt( 1 - e*e * sinLat*sinLat )
+	W = math.sqrt( 1 - e*e * sinLon*sinLon )
 	# Prime vertical radius of curvature
 	N = a / W
 	#
 	h = hei
-	X = ( N + h ) * cosLon * cosLat
-	Y = ( N + h ) * cosLon * sinLat
-	Z = ( N * (1 - e*e) + h ) * sinLon
+	X = ( N + h ) * cosLat * cosLon
+	Y = ( N + h ) * cosLat * sinLon
+	Z = ( N * (1 - e*e) + h ) * sinLat
 	return np.array([X,Y,Z])
+
+# return left-top (latitude,longitude) and right-bottom
+def convertMeshcodeToLatLon( meshcode ):
+	smeshcode = str(meshcode)
+	length = len(smeshcode)
+	lat = int(smeshcode[0:2]) * 2 / 3
+	lon = int(smeshcode[2:4]) + 100
+	lat2 = lat + 2/3
+	lon2 = lon + 1
+	if length > 4:
+		if length >= 6:
+			lat += int(smeshcode[4:5]) * 2 / 3 / 8
+			lon += int(smeshcode[5:6]) / 8
+			lat2 = lat + 2 / 3 / 8
+			lon2 = lon + 1/8
+		if length >= 8:
+			lat += int(smeshcode[6:7]) * 2 / 3 / 8 / 10
+			lon += int(smeshcode[7:8]) / 8 / 10
+			lat2 = lat + 2 / 3 / 8 / 10
+			lon2 = lon + 1 / 8 / 10
+	return [lat2,lon],[lat,lon2]
+
+class VerticesTransformer:
+	def __init__(self, lowerCorner=None, upperCorner=None) -> None:
+		self.rot = np.eye(3)		# rotation matrix 3x3
+		self.trans = np.zeros((3))	# translation vector 3
+		self.scaleX = 1				# scale value of x axis
+		self.aspectXY = 1			# ratio of X / Y
+		if lowerCorner is not None and upperCorner is not None:
+			self.calc( lowerCorner, upperCorner )
+		
+	# calculate rot, trans, scaleX, aspectXY
+	#  lowerCorner, upperCorner must be [lat, lon, 0]
+	def calc(self, lowerCorner, upperCorner):
+		# prepare 3D points correspoinding (0,0), (0,1), (1,0)
+		lt = convertPolarToCartsian(*lowerCorner)
+		rt = convertPolarToCartsian( lowerCorner[0], upperCorner[1], 0 )
+		lb = convertPolarToCartsian( upperCorner[0], lowerCorner[1], 0 )
+		# base point
+		self.trans = copy.deepcopy(lt)
+		# 2 vectors
+		vecx = rt - self.trans
+		vecy = lb - self.trans
+		# aspect ratio X/Y
+		self.aspectXY = np.linalg.norm(vecx) / np.linalg.norm(vecy)
+		# scale X by vecx
+		self.scaleX = 1 / np.linalg.norm(vecx)
+		vecx *= self.scaleX
+		vecy *= self.scaleX
+		# rotation on Z axis
+		angleZ = math.atan2( vecx[1], vecx[0] )
+		rotZ = cv2.Rodrigues( np.array([0,0,-angleZ]) )[0].T
+		# rotation on Y axis
+		angleY = math.atan2( vecx[2], vecx[0]/math.cos(angleZ) )
+		rotY = cv2.Rodrigues( np.array([0,-angleY,0]) )[0].T
+		rot = rotZ.dot( rotY )
+		# apply for vecy
+		vecy = vecy.dot( rot )
+		# rotation on X axis
+		angleX = math.atan2( vecy[2], vecy[1] )
+		rotX = cv2.Rodrigues( np.array([-angleX,0,0]) )[0].T
+		rot = rot.dot( rotX )
+		self.rot = rot
+
+	def transform(self, v, normscale=1, normaspect=True ):
+		vv = (v - self.trans).dot(self.rot)
+		if normscale is not None:
+			vv *= self.scaleX * normscale
+		if normaspect:
+			vv[:,1] *= self.aspectXY
+		return vv
+
+# create Open3D box 
+#  translation (numpy.ndarray[float64[3, 1]]) – A 3D vector to transform the geometry
+def createOpen3Dbox(size=1,translation=None, bLineSet=True):
+	import open3d as o3d
+	mesh = o3d.geometry.TriangleMesh.create_box(width=size,height=size,depth=size)
+	if translation is not None:
+		mesh.translate(translation,relative=False)
+	mesh.compute_vertex_normals()
+	if bLineSet:
+		mesh = o3d.geometry.LineSet.create_from_triangle_mesh(mesh)
+	return mesh
---
+++ test/testVerticesTransformer.py
@@ -0,0 +1,51 @@
+import plateaupy
+import numpy as np
+import open3d as o3d
+
+if __name__ == '__main__':
+	bLoadCache = False		# set it True after the first run
+	#location = 533925
+	location = 533945
+
+	# load
+	pl = plateaupy.plparser(['../CityGML_01','../CityGML_02'])
+	pl.loadFiles( bLoadCache=bLoadCache, cachedir='cached', kind=1, location=location)
+	
+	# target pldem
+	dem = pl.dem[location]
+	# target mesh
+	mesh = dem.meshes[0]
+
+	# get left-top, right-down corners
+	low, upp = plateaupy.plutils.convertMeshcodeToLatLon(location)
+	low.append(0)
+	upp.append(0)
+	print( 'meshcode {}, LT={}, RB={}'.format( location, low, upp ) )
+	
+	# transform mesh.vertices into [0,1]
+	vt = plateaupy.plutils.VerticesTransformer( low, upp )
+	normvertices = vt.transform( mesh.vertices )
+	# replace it to show
+	mesh.vertices = normvertices
+
+	# show by Open3D
+	print('presenting..')
+	window_name = 'test'
+	width  = 640
+	height = 480
+	bgcolor = [1,1,1]
+	vis = o3d.visualization.VisualizerWithKeyCallback()
+	vis.create_window(window_name=window_name, width=width, height=height)
+	vis.get_render_option().background_color = np.asarray(bgcolor)
+	vis.get_render_option().mesh_show_back_face = True
+	vis.get_render_option().mesh_show_wireframe = True
+	vis.get_render_option().show_coordinate_frame = True
+
+	meshes = pl.get_Open3D_TriangleMesh()#color=[0.5,0.5,0.5])
+	for mesh in meshes:
+		vis.add_geometry(mesh)
+	vis.add_geometry( plateaupy.plutils.createOpen3Dbox(size=1) )
+	vis.run()
+	print('ESC to quit.')
+	vis.poll_events()
+	vis.update_renderer()
