ddc36299181d6c112fea50719e3f9666d97ec587
y.sasao <sasao@acculus.jp>
2021-02-17 22:44:28 +0900

    add comment

--- plateaupy/ploptions.py
+++ plateaupy/ploptions.py
@@ -9,4 +9,5 @@ class ploptions:
 		self.bHeightZero = False
 		# divide the mesh 'AAAABB' automatically into the quarter, the group of 'AAAAABBCC'.
 		#  specify (0,0)or(0,1)or(1,0)or(1,1)  as lat,lon
+		#  Now, it is very hard-coded.  Do not use it.
 		self.div6toQuarter = None
